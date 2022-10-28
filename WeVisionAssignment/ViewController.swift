6de1206b0dda111535fc9959245677eda9425855
samuelt37 <samuelt37@gmail.com>
2022-10-27 18:37:34 -0700

    Initial Commit

---
+++ WeVisionAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeVisionAssignment
+//
+//  Created by Samuel Tse on 10/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
