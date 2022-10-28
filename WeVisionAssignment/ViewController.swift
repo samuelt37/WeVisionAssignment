3d34a0603a864100efac298ed5a5e3f0c3a8a61b
Samuel Tse <samuelt37@gmail.com>
2022-10-27 19:24:34 -0700

    inital commit simple animation in mainview
    Signed-off-by: Samuel Tse <samuelt37@gmail.com>

--- WeVisionAssignment/ViewController.swift
+++ WeVisionAssignment/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    var images = [UIImage(named: "ImageA")!,
+                  UIImage(named: "ImageB")!,
+                  UIImage(named: "ImageC")!]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imageView.animationImages = images
+        imageView.animationDuration = 2
+        imageView.animationRepeatCount = 1
     }
-
-
+    
+    @IBAction func startAnimation(_ sender: Any) {
+        imageView.startAnimating()
+    }
+    
+    @IBAction func stopAnimation(_ sender: Any) {
+        imageView.stopAnimating()
+    }
+    
 }
 
